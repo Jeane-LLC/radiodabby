7f11d2d376d4cffa9ec6d811690b817fe5a737bb
Arjun Iyer <arjun.iyer@jeane.llc>
2021-01-08 08:47:42 +0000

    outline for chaotic_mapping.py

---
+++ scripts/chaotic_mapping.py
@@ -0,0 +1,8 @@
+import scipy
+import music21
+
+#read a midi file
+#run an ode45 for the lorenz attractor
+#map the first 12 notes to the first 12 values of the ode45
+#substitute those notes into the midi file
+#write the new midi file with associated metadata
