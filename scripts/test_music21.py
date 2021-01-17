b03192c2e13f3ed5f11333eb1a1ba4a786cf6399
Arjun Iyer <arjun.iyer@jeane.llc>
2021-01-17 16:23:12 +0000

    testing for midi->stream->midi parity
    it seems that the music21.converter does not preserve the original
    midi file's structure...

--- scripts/test_music21.py
+++ scripts/test_music21.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from music21.stream import Stream
 from music21.converter import parse
 from music21.midi.translate import streamToMidiFile
+from filecmp import cmp
 
 
 class TestMusic21(TestCase):
@@ -13,6 +14,14 @@ class TestMusic21(TestCase):
             # part.show("text")
             print("There are " + str(len(part.voices)) + " voices")
             for voice in part.voices:
-                voice.notesAndRests.stream().show("text")
+                # voice.notesAndRests.stream().show("text")
                 pass
         self.assertFalse(stream.isFlat)
+
+    def test_midi_to_midi(self):
+        midiFilename = "/home/arjun/kunstderfuge/satie/satie_gnossienne_1_(c)dery.mid"
+        stream = parse(midiFilename)
+        copyOfMidiFile = stream.write(
+            "midi", fp="/home/arjun/variants/satie_gnossienne_1_copy.mid"
+        )
+        self.assertTrue(cmp(midiFilename, copyOfMidiFile, False))
