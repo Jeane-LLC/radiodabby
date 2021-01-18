833e5d5604bbe4f4e7199754613f0d55cf54a198
Arjun Iyer <arjun.iyer@jeane.llc>
2021-01-18 14:12:59 +0000

    syntax error : where @ should be

--- scripts/ffmpeg_test_render.sh
+++ scripts/ffmpeg_test_render.sh
@@ -3,6 +3,6 @@
 MIDI=$1
 
 echo starting test for $MIDI
-ffmpeg -nostdin -re -i $MIDI -c:a libfdk_aac -profile:a aac_he_v2 -ab 48k -content_type 'audio/aac' -vn -f adts icecast://source:ffmpeg:10.17.0.5:8000/test-render
+ffmpeg -nostdin -re -i $MIDI -c:a libfdk_aac -profile:a aac_he_v2 -ab 48k -content_type 'audio/aac' -vn -f adts icecast://source:ffmpeg@10.17.0.5:8000/test-render
 
 echo done testing $MIDI
